67bb3a21cbef50d49fc91b69f72ce4300981e9a7
kitechoi <chevel0212@gmail.com>
2025-03-03 14:35:24 +0900

    [feat] EventController createEvent 구체화

--- src/main/java/me/celine/demo/events/EventController.java
+++ src/main/java/me/celine/demo/events/EventController.java
@@ -1,9 +1,12 @@
 package me.celine.demo.events;
 
 
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
 
@@ -11,11 +14,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController  {
 
-    @PostMapping("/api/events")
-    public ResponseEntity createEvent() {
-        URI createdUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
-        return ResponseEntity.created(createdUri).build();
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri).body(event);
     }
 }
--- src/test/java/me/celine/demo/events/EventControllerTests.java
+++ src/test/java/me/celine/demo/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package me.celine.demo.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -20,14 +23,34 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("spring comment")
+                .beginEnrollmentDateTime(LocalDateTime.of(2025,3,1, 14,18))
+                .closeEnrollmentDateTime(LocalDateTime.of(2025,3,3,14,18))
+                .beginEventDateTime(LocalDateTime.of(2025,3,4,15,0))
+                .endEventDateTime(LocalDateTime.of(2025,3,6,15,5))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+
+                .build();
+
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
                 )
                 .andDo(print()) // 어떤 요청을 받고, 응답을 줬는지 확인
-                .andExpect(status().isCreated());
-    }
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
 
+
+    }
 }
